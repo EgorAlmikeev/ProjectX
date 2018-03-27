f50848624aafca4d1811cdd208c1b90af346cc20
Sophia Kramar <krasofija@yandex.ru>
2018-03-27 21:08:34 +0300

    4 function titles added

--- MathCore/Algorithms/Integral.h
+++ MathCore/Algorithms/Integral.h
@@ -2,5 +2,9 @@
 #define INTEGRAL_H
 
 double IntLeftRect(char* func, double a, double b, int n);
+double IntRightRect(char* func, double a, double b, int n);
+double IntMedianRect(char* func, double a, double b, int n);
+double IntTrapeze(char* func, double a, double b, int n);
+double IntSimpson(char* func, double a, double b, int n);
 
 #endif // INTEGRAL_H
