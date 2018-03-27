cf4e25a6510f457e0a80c9dbc99e8e9397b7dae5
Sophia Kramar <krasofija@yandex.ru>
2018-03-27 15:04:43 +0300

    + 4 integral algorithms

--- MathCore/Algorithms/Integral.c
+++ MathCore/Algorithms/Integral.c
@@ -6,8 +6,6 @@ double IntLeftRect(char* func, double a, double b, int n)
 {
     double s, h, x, fx;
 
-    //BeginCalc();
-
     s = 0;
 
     h = (b - a) / (double)n;
@@ -30,4 +28,126 @@ double IntLeftRect(char* func, double a, double b, int n)
     }
 
   return h * s;
+}
+
+double IntRightRect(char* func, double a, double b, int n)
+{
+    double s, h, x, fx;
+
+    s = 0;
+
+    h = (b - a) / (double)n;
+    x = a + h;
+
+    while(x < b + h)
+    {
+        fx = FunctionX(func, x);
+        if(IsSyntaxError())
+        {
+            ErrorCalc();
+            return NAN;
+        }
+
+        s = s + fx;
+        x = x + h;
+
+        if(IsCancel())
+            return NAN;
+    }
+
+  return h * s;
+}
+
+double IntMedianRect(char* func, double a, double b, int n)
+{
+    double s, h, x, fx;
+
+    s = 0;
+
+    h = (b - a) / (double)n;
+    x = a + h * 0.5;
+
+    while(x < b)
+    {
+        fx = FunctionX(func, x);
+        if(IsSyntaxError())
+        {
+            ErrorCalc();
+            return NAN;
+        }
+
+        s = s + fx;
+        x = x + h;
+
+        if(IsCancel())
+            return NAN;
+    }
+
+  return h * s;
+}
+
+double IntTrapeze(char* func, double a, double b, int n)
+{
+    double s, h, x, fx;
+    double y1, y2;
+
+    s = 0;
+
+    y1 = FunctionX(func, a);
+    y2 = FunctionX(func, b);
+
+    if(IsSyntaxError())
+        {
+            ErrorCalc();
+            return NAN;
+        }
+
+    x = a + h;
+
+    while(x < b)
+    {
+        fx = FunctionX(func, x);
+        if(IsSyntaxError())
+        {
+            ErrorCalc();
+            return NAN;
+        }
+
+        s = s + fx;
+        x = x + h;
+
+        if(IsCancel())
+            return NAN;
+    }
+
+  return h * ((y1 + y2) / 2 + s);
+}
+
+double IntSimpson(char* func, double a, double b, int n)
+{
+    double s, h;
+    int i;
+
+    h = (b - a) / (double)n;
+
+    s = 0;
+    i = 1;
+
+    while(i <= n - 1)
+    {
+        s = s + FunctionX(func, (a + (i + 1) * h)) + FunctionX(func, (a + (i - 1) * h));// s1
+        s = s + 4 * FunctionX(func, (a + i * h));// s2
+
+        if(IsSyntaxError())
+        {
+            ErrorCalc();
+            return NAN;
+        }
+
+        i = i + 2;
+        if(IsCancel())
+            return NAN;
+    }
+
+  return s * (h / 3);
 }
