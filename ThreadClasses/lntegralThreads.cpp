00f28415632d13bd86952111a2e1af8c5d38d9ee
AnotherStudent <dr.enter256@gmail.com>
2018-03-27 21:46:51 +0300

    Update lntegralThreads.cpp

--- ThreadClasses/lntegralThreads.cpp
+++ ThreadClasses/lntegralThreads.cpp
@@ -1,18 +1,42 @@
 #include "IntegralThreads.h"
 
-IntLeftRectThread::IntLeftRectThread(QString func, double a, double b, int n)
+IntThread::IntThread(QString func, double a, double b, int n, IntMode mode)
 {
     this->func = func;
     this->a = a;
     this->b = b;
     this->n = n;
+    this->mode = mode;
 }
 
-void IntLeftRectThread::run()
+void IntThread::run()
 {
     BaseCalcThread::run();
-
-    double ans = IntLeftRect(QStrToCStr(func), a, b, n);
+    
+    switch(mode)
+    {
+        case ModeIntLeftRect:
+            double ans = IntLeftRect(QStrToCStr(func), a, b, n);
+            break;
+       
+        case ModeIntRightRect:
+            double ans = IntRightRect(QStrToCStr(func), a, b, n);
+            break;
+            
+        case ModeIntMedianRect:
+            double ans = IntMedianRect(QStrToCStr(func), a, b, n);
+            break;
+            
+        case ModeIntTrapeze:
+            double ans = IntTrapeze(QStrToCStr(func), a, b, n);
+            break;
+            
+        case ModeIntSimpson:
+            double ans = IntSimpson(QStrToCStr(func), a, b, n);
+            break;
+        
+        default:;
+    }
 
     if(IsErrorCalc())
     {
