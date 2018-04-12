3c779e3754eb66b46d15b7d63f91f8fc65bc5d89
Sophia Kramar <krasofija@yandex.ru>
2018-04-12 17:01:11 +0300

    заголовки для методов Эйлера и Рунге-Кутта + структура

---
+++ MathCore/Algorithms/Differential.h
@@ -0,0 +1,16 @@
+#ifndef Differential_h
+#define Differential_h
+#include "../MathUtils.h"
+
+typedef struct    //волшебная структура
+{
+    double x, y;
+} TPointF;
+
+typedef TPointF* PointFArray;
+
+double DifferentialEuler(char* func, double x0, double y0, int n, double h, PointFArray m);
+double DifferentialRungeKutt(char* func, double x0, double y0, int n, double h, PointFArray m);
+
+
+#endif /* Differential_h */
