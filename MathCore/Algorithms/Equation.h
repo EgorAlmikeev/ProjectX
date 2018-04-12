ac68cd364b109e7232ce076207ad1a80694b9dc7
Sophia Kramar <krasofija@yandex.ru>
2018-04-12 16:57:46 +0300

    потёрла структуру, переношу всё в файлы Differential.c и Differential.h

--- MathCore/Algorithms/Equation.h
+++ MathCore/Algorithms/Equation.h
@@ -2,11 +2,6 @@
 #define equation_h
 #include "../MathUtils.h"
 
-typedef struct
-{
-    double x, y;
-} TPointF;
-
 double EqHalfDiv(char* func, double a, double b, double epsilon, int* countRef);
 double EqChord(char* func, double a, double b, double epsilon, int* countRef);
 
