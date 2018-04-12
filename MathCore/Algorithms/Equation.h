46f95cd35b789ad6709f2195e2c6c4c3fc95cf1b
Sophia Kramar <krasofija@yandex.ru>
2018-04-12 16:51:33 +0300

    убрала метод Эйлера

--- MathCore/Algorithms/Equation.h
+++ MathCore/Algorithms/Equation.h
@@ -7,10 +7,7 @@ typedef struct
     double x, y;
 } TPointF;
 
-typedef TPointF* PointFArray;
-
 double EqHalfDiv(char* func, double a, double b, double epsilon, int* countRef);
 double EqChord(char* func, double a, double b, double epsilon, int* countRef);
-double DifferentialEuler(char* func, double x0, double y0, int n, double h, PointFArray m);
 
 #endif /* equation_h */
