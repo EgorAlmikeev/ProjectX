c11a6cc9a4b55401f6058e6be5f2bd544fcdd0bb
Sophia Kramar <krasofija@yandex.ru>
2018-04-12 13:03:59 +0300

    DifferentialEuler header added

--- MathCore/Algorithms/Equation.h
+++ MathCore/Algorithms/Equation.h
@@ -1,7 +1,10 @@
 #ifndef equation_h
 #define equation_h
+#include "../MathUtils.h"
+typedef double** TArrayXY;
 
-double EqHalfDiv(char* func, double a, double b, double epsilon, int* countRef);
-double EqChord(char* func, double a, double b, double epsilon, int* countRef);
+double HalfDiv(char* func, double a, double b, double epsilon, int* countRef);
+double Corde(char* func, double a, double b, double epsilon, int* countRef);
+double DifferentialEuler(char* func, double x0, double y0, int n, double h, TArrayXY m);
 
 #endif /* equation_h */
